409d1f2aa6b2b56b1008430fcc74d3e935ca4db3
peterberwind <peterberwind@gmail.com>
2016-05-02 19:34:57 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+
+  # def init(dog_name)
+  #   @name = dog_name
+  # end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    "#{@name}"
+  end
+
+  # def init(dog_breed)
+  #   @breed = dog_breed
+  # end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    "#{@breed}"
+  end
+
+end
